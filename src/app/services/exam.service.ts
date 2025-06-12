0c0efe9dd12373a2078dfa539c980d4275e67324
FelopaterAdel <felopateradel@yahoo.com>
2025-06-12 12:40:44 +0300

    Exam , result services

---
+++ src/app/services/exam.service.ts
@@ -0,0 +1,44 @@
+import { IExam } from '../models/IExam';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from './AuthService';
+
+@Injectable({
+
+  providedIn: 'root'
+
+})
+export class examService {
+  baseUrl: string = "http://localhost:3000/api/exams"
+  constructor(private http: HttpClient,private authService:AuthService) { }
+
+  getAllExams(): Observable<IExam[]> {
+    const headers = new HttpHeaders({
+      'Authorization':  `${this.authService.currentToken}`,
+    });
+
+    return this.http.get<IExam[]>(this.baseUrl,{ headers });
+  }
+
+  getExamById(examId: string): Observable<IExam> {
+    return this.http.get<IExam>(`${this.baseUrl}/${examId}`,)
+  }
+  createExam(exam: IExam): Observable<IExam> {
+    const headers = new HttpHeaders({
+      'Authorization':  `${this.authService.currentToken}`,
+    });
+    return this.http.post<IExam>(`${this.baseUrl}`, exam ,{ headers })
+
+  }
+
+  editExam(examId: string, exam: any): Observable<IExam> {
+    return this.http.put<IExam>(`${this.baseUrl}/${examId}`, exam);
+  }
+
+  deleteExam(examId: string): Observable<IExam> {
+    console.log('Sending request with ID:', examId);
+    return this.http.delete<IExam>(`${this.baseUrl}/${examId}`);
+
+  }
+}
---
+++ src/app/services/result.service.ts
@@ -0,0 +1,45 @@
+import { IExamResult } from './../models/IExamResult';
+import { AuthService } from './AuthService';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs';
+import { throwError } from 'rxjs';
+
+@Injectable({
+
+  providedIn: 'root'
+
+})
+export class resultService {
+  baseUrl: string = "http://localhost:3000/api/results"
+  constructor(private http: HttpClient, private authService: AuthService) { }
+
+ submitExam(examId: string, answers: any): Observable<IExamResult> {
+  const headers = new HttpHeaders({
+    'Authorization': `Bearer ${this.authService.currentToken}`,
+    'Content-Type': 'application/json'
+  });
+  
+  const payload = {
+    answers: answers
+  };
+  
+  return this.http.post<IExamResult>(`${this.baseUrl}/${examId}`, payload, { headers });
+}
+  getResult(examId: string): Observable<IExamResult> {
+  const headers = new HttpHeaders({
+    'Authorization': `Bearer ${this.authService.currentToken}`
+  });
+  
+  return this.http.get<IExamResult>(`${this.baseUrl}/${examId}`, { headers })
+    .pipe(
+      catchError(error => {
+        console.error('Failed to get exam result:', error);
+        return throwError(error);
+      })
+    );
+}
+
+
+}
