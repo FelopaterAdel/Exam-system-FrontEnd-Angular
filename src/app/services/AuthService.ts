065b0326961da053248e1589a6f547b4a744f125
FelopaterAdel <felopateradel@yahoo.com>
2025-06-12 17:17:15 +0300

    services update

--- src/app/services/AuthService.ts
+++ src/app/services/AuthService.ts
@@ -75,4 +75,5 @@ export class AuthService {
   isAuthenticated() {
     return !!this.currentToken;
   }
+  
 }
--- src/app/services/authorezation.ts
+++ src/app/services/authorezation.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/User';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from './AuthService';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +10,10 @@ export class authorezationService {
 
   baseUrl: string = "http://localhost:3000/api/auth";
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private authService:AuthService
+  ) { }
 
   registration(user: User): Observable<any> {
     return this.http.post(`${this.baseUrl}/register`, user);
@@ -20,6 +23,12 @@ export class authorezationService {
       const body = { email, password };
 
     return this.http.post(`${this.baseUrl}/login`, body);
+  }
+  getUser(): Observable<any> {
+    const headers = new HttpHeaders({
+      'Authorization':  `${this.authService.currentToken}`,
+    });
+    return this.http.get(`${this.baseUrl}/user`,{ headers });
   }
 
 }
